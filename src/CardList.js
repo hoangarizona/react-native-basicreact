bc98ae8f77b3c95005d4b11d64208f988320e2be
hoangarizona <hoangarizona@gmail.com>
2022-12-05 15:36:23 -0700

    CardList 3

--- src/CardList.js
+++ src/CardList.js
@@ -2,18 +2,21 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    const cardComponent = robots.map((user, i) => {
-        return (
-            <Card
-                key={robots[i].id}
-                id={robots[i].id} name={robots[i].name}
-                email={robots[i].email} />
-        );
-    });//this is an array of Cards
+  
  
     return (
         <div>
-            {cardComponent};
+            {
+            robots.map((user, i) => {
+                return (
+                    <Card
+                        key={robots[i].id}
+                        id={robots[i].id}
+                        name={robots[i].name}
+                    email={robots[i].email}
+                />);
+                })//this is an array of Cards
+            }
         </div>
         );
 }
