441624ad703bb7fd97524e1476de06dc1fde5ce1
hoangarizona <hoangarizona@gmail.com>
2022-12-04 21:47:18 -0700

    Card

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-
-        <Hello greeting={'Hello React Ninja'}/>
+        <div>
+            <Card />
+            <Card />
+            <Card />
+        </div>
     </React.StrictMode>
 );
 
