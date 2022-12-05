5703bef1ebc6395eb762010627722a2991917ae7
hoangarizona <hoangarizona@gmail.com>
2022-12-04 21:12:53 -0700

    function hello

--- src/Hello.js
+++ src/Hello.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Hello.css';
-class Hello extends Component {
-    render() {
-        return (
-            <div className="f1 tc">
-                <h1>Hello World</h1>
-                <p>{ this.props.greeting}</p>
-            </div >
-        );//expect to return a single thing
-    }
+const Hello = (props)=> {
+    return (
+        <div className="f1 tc">
+            <h1>Hello World</h1>
+            <p>{ props.greeting}</p>
+        </div >
+    );//expect to return a single thing
+    
 }
 export default Hello;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
 
-        <Hello greeting={'Hello' + ' React Ninja'}/>
+        <Hello greeting={'Hello React Ninja'}/>
     </React.StrictMode>
 );
 
