0082b568f6189844a2e4034b9e614f9fdb4befd5
hoangarizona <hoangarizona@gmail.com>
2022-12-05 15:25:52 -0700

    CardList

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
+import CardList from './CardList';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 import { robots } from './robots';//have to destruct because the export of robots.js aren't default'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-        </div>
+        
+        <CardList robots={ robots}></CardList>
     </React.StrictMode>
 );
 
