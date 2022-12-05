7f2304b0b32d6ca3080c638896fe28aaf03e9c0c
hoangarizona <hoangarizona@gmail.com>
2022-12-04 20:59:25 -0700

    class component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Hello.css';
+class Hello extends Component {
+    render() {
+        return (
+            <div className="f1 tc">
+                <h1>Hello World</h1>
+                <p>{ this.props.greeting}</p>
+            </div >
+        );//expect to return a single thing
+    }
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+
+        <Hello greeting={'Hello' + ' React Ninja'}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
