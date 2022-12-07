ca4887ff7b83bf37ca1b1c1428416d375a7b98b0
hoangarizona <hoangarizona@gmail.com>
2022-12-07 15:26:36 -0700

    props.children

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import CardList from './CardList';
 import { robots } from './robots';//have to destruct because the export of robots.js aren't default'
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css'
 
 
@@ -44,7 +45,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
-                    <CardList robots={filteredRobots} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
                 </div>
             );
         }
